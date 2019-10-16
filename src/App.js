87551c27c610f4bfa98693cd167f4ce7a5def64b
Caleb Redd <caleb.redd@yahoo.com>
2019-10-16 03:09:51 -0400

    Set up backbone CONTEXT-API files, Provider implemented in Appjs but not on Consumer side

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./App.css";
+import UserContext from "./contexts/UserContext";
+import EventContext from "./contexts/EventContext";
+
 function App() {
+  const event = useContext(EventContext);
+  const user = useContext(UserContext);
+
   return (
-    <div className="App">
-      <h1>In Playground.</h1>
-    </div>
+    <EventContext.Provider value={event}>
+      <UserContext.Provider value={user}>
+        <div className="App">
+          <h1>In Playground.</h1>
+        </div>
+      </UserContext.Provider>
+    </EventContext.Provider>
   );
 }
 
