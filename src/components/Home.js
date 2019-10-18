ce5bff647a449e48f408ee94c4d6ac78c14705f7
Christopher Lien <cnlien@gmail.com>
2019-10-18 16:52:34 -0400

    committing styles and structure to the homepage dashboard and sidebar navigation

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,11 @@ import {Container, Col, Row, Button} from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.css';
 import './Home.css';
 
+import SidebarCreateEvent from '../photos/Icons/create-event-sidebar.svg';
+import SidebarPotluckInvite from '../photos/Icons/potluck-invite-sidebar.svg';
+import SidebarDashboardIcon from '../photos/Icons/dashboard-eye-sidebar.svg';
+import SidebarLogoutIcon from '../photos/Icons/logout-sidebar.svg';
+
 const Home = () => {
   const handleLogout = () => {
     localStorage.removeItem('token')
@@ -13,8 +18,17 @@ const Home = () => {
     <>
       <div className="dashboard-container">
         <div className="dashboard-sidebar">
-          <h4>Welcome Back, First Name</h4>
-          <Button className="logout-button" onClick={handleLogout}>Log Out</Button>
+          <div className="dashboard-sidebarContainer">
+            <h4 className="dashboard-welcome">Welcome Back, First Name</h4>
+
+            <h4 className="dashboard-actions"><img className="sidebarIcon" src={SidebarCreateEvent} alt="Create Event Icon"/>Create Event</h4>
+            <h4 className="dashboard-actions"><img className="sidebarIcon" src={SidebarPotluckInvite} alt="Create Event Icon"/>No Potluck Invite</h4>
+            <hr />
+
+            <Button className="dashboard-button sidebarButton"><img className="sidebarIcon buttonIcon" src={SidebarDashboardIcon} alt="Create Event Icon"/>Dashboard</Button>
+            <Button className="dashboard-logout sidebarButton" onClick={handleLogout}><img className="sidebarIcon buttonIcon" src={SidebarLogoutIcon} alt="Create Event Icon"/>Log Out</Button>
+
+          </div>
         </div>
         
         <div className="dasboard-content">
